b6b3a2118c13f3f92d285ee33ac2784a4c726f6b
iqbalmuhammad08f <iqbalnurhakimy83@gmail.com>
2025-01-05 21:19:43 +0700

    membuat menu

--- simple library management system/Program.cs
+++ simple library management system/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Sipepes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("== Sistem Management Perpustakaan ==\n");
+            Console.WriteLine("1. Tambah Buku");
+            Console.WriteLine("2. Lihat Daftar Buku");
+            Console.WriteLine("3. Cari Buku");
+            Console.WriteLine("4. Hapus Buku");
+            Console.WriteLine("0. Keluar");
+
+        }
+    }
+}
